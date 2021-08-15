4e5eb0fe3046985311719e1332c058f1d66b841c
Dmitry <Dmitrytbp@gmail.com>
2021-08-15 12:46:09 +0300

    Added register and home routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/views/Home'
+import Register from '@/views/Register'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  }
+]
 
 const router = new VueRouter({
   routes

942ea61c44b9d63e61276d4711dbdb66ef546022
Dmitry <Dmitrytbp@gmail.com>
2021-09-10 07:04:59 +0300

    Create ValidationErrors.vue

---
+++ src/components/ValidationErrors.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="error-messages">
+    <li v-for="errorMesage in errorsMessages" :key="errorMesage">
+      {{ errorMesage }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'McvValidationErrors',
+  props: {
+    validationErrors: {
+      type: Object,
+      require: true
+    }
+  },
+  computed: {
+    errorsMessages() {
+      return Object.keys(this.validationErrors).map(name => {
+        const messages = this.validationErrors[name].join(', ')
+        return `${name} ${messages}`
+      })
+    }
+  }
+}
+</script>

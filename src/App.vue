2a956788ea18ddf1919d55a70e19c69dacda1a17
Dmitry <Dmitrytbp@gmail.com>
2021-10-02 09:40:54 +0300

    Added topbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
     hello medium!!
+    <mcv-topbar></mcv-topbar>
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar'
+
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  }
+}
+</script>

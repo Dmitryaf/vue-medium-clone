9755c451ad686db8626eb34b45caa0c8029a1bfe
Dmitry <Dmitrytbp@gmail.com>
2021-10-07 07:27:30 +0300

    Added dispatch for getting feed

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -3,10 +3,15 @@
 </template>
 
 <script>
+import { actionTypes } from '@/store/modules/feed'
+
 export default {
   name: 'McvFeed',
   props: {
     apiUrl: { type: String, required: true }
+  },
+  mounted() {
+    this.$store.dispatch(actionTypes.getFeed, { apiUrl: this.apiUrl })
   }
 }
 </script>

08016f3206087e9e6c4702f87530dd29c1aa223f
Dmitry <Dmitrytbp@gmail.com>
2021-10-12 07:56:33 +0300

    Add feed template

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,14 +1,68 @@
 <template>
-  <div>Feed {{ apiUrl }}</div>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error"></div>
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{
+              name: 'userProfile',
+              params: { slug: article.author.username }
+            }"
+          >
+            <img :src="article.author.img" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: { slug: article.author.username }
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="data">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            Add to favorites
+          </div>
+        </div>
+        <router-link
+          :to="{ name: 'article', params: { slug: article.slug } }"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
+  </div>
 </template>
 
 <script>
 import { actionTypes } from '@/store/modules/feed'
+import { mapState } from 'vuex'
 
 export default {
   name: 'McvFeed',
   props: {
     apiUrl: { type: String, required: true }
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    })
   },
   mounted() {
     this.$store.dispatch(actionTypes.getFeed, { apiUrl: this.apiUrl })

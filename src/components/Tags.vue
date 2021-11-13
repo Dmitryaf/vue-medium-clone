53f0fb8a1d274a52fe7d12a22d7222a8ddb4bde7
Dmitry <Dmitrytbp@gmail.com>
2021-11-13 10:02:02 +0300

    Create Tags.vue

---
+++ src/components/Tags.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happened</div>
+    <div v-if="tags" class="sidebar">
+      <p>Popular Tags</p>
+      <div class="tag-list">
+        <router-link
+          v-for="tag in tags"
+          :key="tag"
+          :to="{ name: 'tag', params: { slug: tag } }"
+          class="tag-default tag-pill"
+          >{{ tag }}</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+import { actionTypes } from '@/store/modules/tags'
+
+export default {
+  name: 'McvTags',
+  computed: {
+    ...mapState({
+      isLoading: state => state.tags.isLodaing,
+      tags: state => state.tags.data,
+      error: state => state.tags.error
+    })
+  },
+  mounted() {
+    this.fetchTags()
+  },
+  methods: {
+    fetchTags() {
+      this.$store.dispatch(actionTypes.getTags)
+    }
+  }
+}
+</script>

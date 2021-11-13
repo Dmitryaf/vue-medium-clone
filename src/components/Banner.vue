51c0887218afcd9dc3c7520ccbb1ccdb36023a33
Dmitry <Dmitrytbp@gmail.com>
2021-11-13 10:22:50 +0300

    Create Banner.vue

---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="banner">
+    <div class="container">
+      <h1>Medium clone</h1>
+      <p>Description</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvBanner'
+}
+</script>

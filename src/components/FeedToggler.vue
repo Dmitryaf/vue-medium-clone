43bb9b4e296ce3992a657be59261b37c3eefd3df
Dmitry <Dmitrytbp@gmail.com>
2021-11-16 07:56:52 +0300

    Create FeedToggler.vue

---
+++ src/components/FeedToggler.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="feed-toggle">
+    <ul class="nav nav-pills outline-active">
+      <li v-if="isLoggedIn" class="nav-item">
+        <router-link
+          :to="{ name: 'youFeed' }"
+          class="nav-link"
+          :class="{ active: routeName === 'yourFeed' }"
+          >Your feed</router-link
+        >
+      </li>
+      <li class="nav-item">
+        <router-link
+          :to="{ name: 'globalFeed' }"
+          class="nav-link"
+          :class="{ active: routeName === 'globalFeed' }"
+          >Global feed</router-link
+        >
+      </li>
+      <li class="nav-item" v-if="tagName">
+        <router-link
+          :to="{ name: 'tag' }"
+          class="nav-link"
+          :class="{ active: routeName === 'tag' }"
+        >
+          <i class="ion-pound" />
+          {{ tagName }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import { getterTypes } from '@/store/modules/auth'
+export default {
+  name: 'McvFeedToggler',
+  props: {
+    tagName: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      isLoggedIn: getterTypes.isLoggedIn
+    }),
+    routeName() {
+      return this.$route.name
+    }
+  }
+}
+</script>

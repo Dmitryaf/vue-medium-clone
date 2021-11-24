c7b7a8933ec272fc4aeed974dbf64e6b0c8d12fb
Dmitry <Dmitrytbp@gmail.com>
2021-11-24 07:44:47 +0300

    Added onSubmit logic. Added validationErrors and isSubmitting from state

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -9,6 +9,9 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
+import { actionTypes } from '@/store/modules/createArticle'
+
 import McvArticleForm from '@/components/ArticleForm'
 export default {
   name: 'McvCreateArticle',
@@ -22,14 +25,23 @@ export default {
         description: '',
         body: '',
         tagList: []
-      },
-      validationErrors: null,
-      isSubmitting: false
+      }
     }
   },
+  computed: {
+    ...mapState({
+      isSubmitting: state => state.createArticle.isSubmitting,
+      validationErrors: state => state.createArticle.validationErrors
+    })
+  },
   methods: {
-    onSubmit(data) {
-      console.log(data)
+    onSubmit(articleInput) {
+      this.$store
+        .dispatch(actionTypes.createArticle, { articleInput })
+        .then(article => {
+          this.$router.push({ name: 'article', params: { slug: article.slug } })
+        })
+      console.log(articleInput)
     }
   }
 }
